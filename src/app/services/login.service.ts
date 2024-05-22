967213810b00b8b39892e34a13716a61a891036c
Gabriel Bressi <gabriel@C11-I5BLCMJN31J.dir.svc.accenture.com>
2024-05-22 14:12:43 -0300

    feat: create auth-guard

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,11 +7,21 @@ import { tap } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
+  apiUrl: string = "http://localhost:8080/auth"
 
   constructor(private httpClient: HttpClient) { }
 
-  login(name: string, password: string) {
-    return this.httpClient.post<LoginRespose>("/login", {name, password}).pipe(
+  login(email: string, password: string) {
+    return this.httpClient.post<LoginRespose>(this.apiUrl + "/login", {email, password}).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+
+  signup(name: string, email: string, password: string) {
+    return this.httpClient.post<LoginRespose>(this.apiUrl + "/register", {name, email, password}).pipe(
       tap((value) => {
         sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
